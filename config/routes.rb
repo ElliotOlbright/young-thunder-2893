bcfd5856aef3e85c891627bc627f1e5967c64e77
Elliot Olbright <elliotolbright@gmail.com>
2021-07-19 10:26:59 -0600

    passing link movie show link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/studios', to: 'studio#index'
+  get '/movies/:id/:id', to: 'movie#show'
   get '/studios/:id', to: 'studio#show'
 end
